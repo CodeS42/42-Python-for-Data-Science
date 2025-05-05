cee9a2af4f9d3c509dc97291952af45a673e347b
Serena Gandum--Diagne <sgandum-@bess-f2r6s3.clusters.42paris.fr>
2025-05-05 19:45:18 +0200

    Python-0-Starting ex06 done

---
+++ Python-0-Starting/ex06/filterstring.py
@@ -0,0 +1,24 @@
+import sys
+from ft_filter import ft_filter
+
+def valid_arguments(args):
+    try:
+        int(sys.argv[2])
+        str(sys.argv[1])
+    except ValueError:
+        return False
+    return True
+
+def main():
+    try:
+        if not len(sys.argv) == 3 or not valid_arguments(sys.argv[1:]):
+            raise AssertionError("AssertionError: the arguments are bad")
+        N = int(sys.argv[2])
+        S = str(sys.argv[1]).split()
+        filterstring = lambda word: len(word) > N
+        print(ft_filter(filterstring, S))
+    except AssertionError as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
---
+++ Python-0-Starting/ex06/ft_filter.py
@@ -0,0 +1,2 @@
+def ft_filter(func, iterable_value):
+    return [element for element in iterable_value if func(element)]
