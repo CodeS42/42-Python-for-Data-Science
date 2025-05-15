aee7574002e9a47a9cfc6a41641a346edc26c0c4
Scode0 <gandumdiagne.serena@gmail.com>
2025-05-15 05:54:28 +0200

    Python-0-Starting ex08 implement loading bar

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -5,9 +5,14 @@ def ft_tqdm(lst: range) -> None:
     count = 0
 
     for elem in lst:
+        loading_bar = [
+
+        ]
         count += 1
         if count == print_block:
-            print(f"\r{str(print_percent)}%", end="", flush=True)
+            # f"100%|████████████████████████| 333/333 [00:01<00:00, 171.42it/s]"
+            f"{percent}%|{loading_bar}| {passed_elements}/{total_elements} [00:01<00:00, 171.42it/s]"
+            print(f"\r{str(print_percent)}%|", end="", flush=True)
             print_percent += step_percent
             print('\u2588', end="", flush=True)
             count = 0
