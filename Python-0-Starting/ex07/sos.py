20e0d7cbb11efb4966b7ef2381c0097447a6572e
Serena Gandum--Diagne <sgandum-@f2r9s14.clusters.42paris.fr>
2025-11-29 20:45:01 +0100

    Python-0-Starting ex07 ready to be validated

--- Python-0-Starting/ex07/sos.py
+++ Python-0-Starting/ex07/sos.py
@@ -1,5 +1,6 @@
 import sys
 
+
 NESTED_MORSE = {
     " ": "/ ",
     "A": ".- ",
@@ -40,11 +41,15 @@ NESTED_MORSE = {
     "0": "----- "
 }
 
+
 def wrong_character(S):
+    if not S:
+        return True
     for c in S:
         if not c.isalpha() and not c.isdigit() and not c.isspace():
             return True
     False
+
 
 def convert_string(S):
     new_string = ""
@@ -52,17 +57,20 @@ def convert_string(S):
 
     for c in S:
         new_string += NESTED_MORSE[c]
-    
+    new_string = new_string[:-1]
+
     return new_string
+
 
 def main():
     try:
         if len(sys.argv) != 2 or wrong_character(sys.argv[1]):
             raise AssertionError("AssertionError: the arguments are bad")
-        
+
         print(convert_string(sys.argv[1]))
     except AssertionError as e:
         print(e)
-    
+
+
 if __name__ == "__main__":
     main()
