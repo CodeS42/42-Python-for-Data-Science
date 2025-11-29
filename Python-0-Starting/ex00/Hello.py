09f0d6479be710dadaaabb5876e04c4cbb066b4d
Serena Gandum--Diagne <sgandum-@f2r9s14.clusters.42paris.fr>
2025-11-29 16:04:53 +0100

    Python-0-Starting ex00 ready to be validated

--- Python-0-Starting/ex00/Hello.py
+++ Python-0-Starting/ex00/Hello.py
@@ -5,8 +5,9 @@ ft_dict = {"Hello" : "titi!"}
 
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "France!")
-ft_set = "{'Hello', 'Paris!'}"
-ft_dict["Hello"] = "42Paris"
+ft_set -= {"tutu!"}
+ft_set |= {"Paris!"}
+ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
 print(ft_tuple)
