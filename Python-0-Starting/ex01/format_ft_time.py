465b412e4b17a8390c5d2161da41d271e7228c72
Serena Gandum--Diagne <sgandum-@f2r9s14.clusters.42paris.fr>
2025-11-29 16:14:24 +0100

    Python-0-Starting ex01 ready to be validated

--- Python-0-Starting/ex01/format_ft_time.py
+++ Python-0-Starting/ex01/format_ft_time.py
@@ -2,5 +2,5 @@ from time import time
 from datetime import datetime
 
 timestamp = time()
-print("Seconds since January 1, 1970: " + str(timestamp) + f" or {timestamp:.2e} in scientific notation")
+print(f"Seconds since January 1, 1970: {timestamp:,.4f} or {timestamp:.2e} in scientific notation")
 print(datetime.now().strftime("%b %d %Y"))
