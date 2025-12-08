985fb287866848a0b73d1eab67cc61785361c3f4
Serena Gandum--Diagne <sgandum-@f2r4s17.clusters.42paris.fr>
2025-12-08 17:54:35 +0100

    Python-2-DataTable: ex02 done

--- Python-2-DataTable/ex02/aff_pop.py
+++ Python-2-DataTable/ex02/aff_pop.py
@@ -2,13 +2,33 @@ from load_csv import load
 import matplotlib.pyplot as plt
 
 
+def filter_data(country, df, years):
+    """
+
+    """
+    country_data = df[df["country"] == country].drop(columns=["country"])
+    country_data = country_data.replace({"k": "e3", "M": "e6"}, regex=True).astype(float)
+    country_data = country_data[years]
+    country_data = country_data.iloc[0]
+    return country_data
+
+
 def aff_pop(df):
     """
 
     """
+    years = [year for year in df.columns[1:] if 1800 <= int(year) <= 2050]
+    france_data = filter_data("France", df, years)
+    belgium_data = filter_data("Belgium", df, years)
     
+    plt.plot(france_data.index, france_data.values, label="France", color="green")
+    plt.plot(belgium_data.index, belgium_data.values, label="Belgium")
+    plt.yticks([20000000, 40000000, 60000000], ["20M", "40M", "60M"])
+    plt.xticks(years[::40])
+    plt.title("Population Projections")
     plt.ylabel("Population")
     plt.xlabel("Year")
+    plt.legend(loc="lower right")
     plt.show()
 
 
