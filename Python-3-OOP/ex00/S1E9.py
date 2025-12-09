3fdb526317ef5a12dc66a1e194f2c73d13e3c31d
Serena Gandum--Diagne <sgandum-@f2r4s6.clusters.42paris.fr>
2025-12-09 17:37:08 +0100

    Python-3-OOP ex00: start to implement first class

---
+++ Python-3-OOP/ex00/S1E9.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Your docstring for Class"""
+    def __init__(self, first_name, ):
+        self.first_name = first_name
+
+
+class Stark(Character):
+    """Your docstring for Class"""
+    def __init__(self, )
